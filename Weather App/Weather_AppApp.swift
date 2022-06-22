3a393468aea45da7e018680d6732068fbea314ca
RezEnayati <94985316+RezEnayati@users.noreply.github.com>
2022-06-22 13:56:24 -0400

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Reza Enayati on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
